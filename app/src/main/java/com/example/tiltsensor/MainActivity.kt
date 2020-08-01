a0db5fe5ccf81b01166915e780b9cf6feea6b91c
junminChang <chjm219@kookmin.ac.kr>
2020-08-02 00:58:54 +0900

    onSensorChanged

--- app/src/main/java/com/example/tiltsensor/MainActivity.kt
+++ app/src/main/java/com/example/tiltsensor/MainActivity.kt
@@ -3,15 +3,19 @@ package com.example.tiltsensor
 import android.content.Context
 import android.hardware.Sensor
 import android.hardware.Sensor.TYPE_ACCELEROMETER
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private val sensorManager: SensorManager by lazy {
         getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +29,21 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         sensorManager.registerListener(this, sensorManager.getDefaultSensor(TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
     }
-    
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+
+        p0?.let {
+            Log.d("MainActivity", "onSensorChanged: x :+ ${p0.values[0]}, y: ${p0.values[1]}, z: ${p0.values[2]}"  )
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
 
 }
